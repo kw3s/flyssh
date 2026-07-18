c494b3477f2e9efc1c49bdc10d0785585796c6d0
kw3s <119457325+kw3s@users.noreply.github.com>
2026-07-19 00:07:20 +0200

    Ensure SSH user setup occurs unconditionally

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,15 +3,15 @@ set -e
 
 if ! id sshuser >/dev/null 2>&1; then
     useradd -m sshuser
-    mkdir -p /home/sshuser/.ssh
-    echo "$SSH_PUBLIC_KEY" > /home/sshuser/.ssh/authorized_keys
-    chown -R sshuser:sshuser /home/sshuser/.ssh
-    chmod 700 /home/sshuser/.ssh
-    chmod 600 /home/sshuser/.ssh/authorized_keys
 fi
-mkdir -p /var/run/sshd
 
-# Generate host keys if they don't already exist
+mkdir -p /home/sshuser/.ssh
+echo "$SSH_PUBLIC_KEY" > /home/sshuser/.ssh/authorized_keys
+chown -R sshuser:sshuser /home/sshuser/.ssh
+chmod 700 /home/sshuser/.ssh
+chmod 600 /home/sshuser/.ssh/authorized_keys
+
+mkdir -p /var/run/sshd
 ssh-keygen -A
 
 exec /usr/sbin/sshd -D -e
