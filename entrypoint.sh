2157cc216dcbd318dfe0c89cf9ecf03cf156d046
kw3s <119457325+kw3s@users.noreply.github.com>
2026-07-28 00:25:59 +0200

    Remove SSH user setup from entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,1 @@
-#!/bin/bash
-set -e
 
-if ! id sshuser >/dev/null 2>&1; then
-    useradd -m sshuser
-fi
-
-mkdir -p /home/sshuser/.ssh
-echo "$SSH_PUBLIC_KEY" > /home/sshuser/.ssh/authorized_keys
-chown -R sshuser:sshuser /home/sshuser/.ssh
-chmod 700 /home/sshuser/.ssh
-chmod 600 /home/sshuser/.ssh/authorized_keys
-
-mkdir -p /var/run/sshd
-ssh-keygen -A
-
-exec /usr/sbin/sshd -D -e
